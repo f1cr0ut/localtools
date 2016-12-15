3b86ec99775ee3e6620c9d37b193be49bd3467a7
f1cr0ut <f1cr0ut.medour@gmail.com>
2016-12-15 11:11:51 +0900

    display finished or not

--- view_tickets.py
+++ view_tickets.py
@@ -25,20 +25,24 @@ def MainDisp(db_name, show_detail):
 		return
 	conn = sqlite3.connect(db_name)
 	conn.cursor()
-	sql = u"select id, priority, title, body, progress, deadline, category from ticket where deleted=0 order by priority desc;"
-	bodynum = 3
-	prioritynum = 1
-	progressnum = 4
-	deadlinenum = 5
-	categorynum = 6
+	sql = u"select id, finished, priority, title, body, progress, deadline, category from ticket where deleted=0 order by priority desc;"
+	bodynum = 4
+	prioritynum = 2
+	progressnum = 5
+	deadlinenum = 6
+	categorynum = 7
+	finishednum = 1
 	for row in conn.execute(sql):
 		print_str = "| "
 		count = 0
 		for c in row:
 			if count == progressnum or count == deadlinenum or count == categorynum or count == bodynum:
+				count += 1
 				continue
 			if count == prioritynum:
 				print_str += "pri:" + str(c).replace('\n','') + " | "
+			elif count == finishednum:
+				print_str += "[完了] | " if c == int(1) else ""
 			else:
 				print_str += str(c).replace('\n','') + " | "
 			count += 1
@@ -68,7 +72,7 @@ def MainDisp(db_name, show_detail):
 				Println("追記" + str(nnum) +": (" + str(rr[1]) + ")")
 				detail = rr[0].replace('\r\n', '\n')
 				detail = detail.replace('\r', '\n')
-				Println('\n\t' + detail.replace('\n', '\n\t') + '\n')
+				Println('\t' + detail.replace('\n', '\n\t'))
 				nnum += 1
 			Println('')
 	conn.close()
