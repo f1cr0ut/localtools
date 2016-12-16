77fdb51a8ee54795efd9e6fcc265521530ea87e7
f1cr0ut <f1cr0ut.medour@gmail.com>
2016-12-16 13:11:59 +0900

    add documentDB

---
+++ CreateDBsql/CreateDocumentDB.sql
@@ -0,0 +1,31 @@
+CREATE TABLE `document` (
+    `id`                INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+    `title`             TEXT,
+    `description`       TEXT,
+    `deleted`           INTEGER NOT NULL DEFAULT 0
+);
+
+CREATE TABLE `section` (
+    `id`                INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+    `document_id`       INTEGER NOT NULL,
+    `title`             TEXT,
+    `body`              TEXT NOT NULL,
+    `section_list_id`   INTEGER,
+    `tag_list_id`       INTEGER,
+    `deleted`           INTEGER NOT NULL DEFAULT 0
+);
+
+CREATE TABLE `section_list` (
+    `id`                INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+    `section_first`     INTEGER NOT NULL UNIQUE,
+    `section_second`    INTEGER NOT NULL UNIQUE,
+    `section_third`     INTEGER NOT NULL UNIQUE,
+    `section_fourth`    INTEGER NOT NULL UNIQUE,
+    `deleted`           INTEGER NOT NULL DEFAULT 0
+);
+
+CREATE TABLE `tag_list` (
+    `id`                INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+    `name`              TEXT NOT NULL UNIQUE,
+    `deleted`           INTEGER NOT NULL DEFAULT 0
+);
